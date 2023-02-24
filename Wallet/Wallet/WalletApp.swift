ec135b257070f610763083578c44865c56d37b9b
Eugene Mokeiev <ievgen.mokeiev@justeattakeaway.com>
2023-02-24 14:33:41 +0200

    initial

---
+++ Wallet/Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Yevhen Mokeiev on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
